166e0c1674ececa9bc982ea12fba9befc774d4af
Pascal <pascalslogin@gmail.com>
2016-05-09 22:04:38 -0700

    folder structure

---
+++ raspberry/index.js
@@ -0,0 +1,9 @@
+
+var Player = require('player');
+
+var player = new Player("https://p.scdn.co/mp3-preview/6f1164ab51b257afed6247d10f804cc25bcd3c5c");
+
+player.play(function(err, player) {
+	console.log("end");
+	}
+	);
