5d250f56e6a31030af438b78ed245a89e34b72f8
Pascal C <pascalslogin@gmail.com>
2016-05-20 22:12:10 -0700

    raspberry server update, now supports info

--- raspberry/app2.js
+++
@@ -1,8 +0,0 @@
-var koa = require('koa');
-var app = koa();
-
-app.use(function *(){
-  this.body = 'Hello World';
-});
-
-app.listen(80, '0.0.0.0');
--- raspberry/index.js
+++ raspberry/index.js
@@ -1,22 +1,42 @@
 // Create an app
 var Player = require('player');
 var server = require('diet');
+var request = require('request');
 var app = server();
 app.listen('http://0.0.0.0:80');
 var player;
 var client_id = process.env["SOUNDCLOUD_ALEXA_BRIDGE_CLIENT_ID"];
 if (!client_id) {throw "Missing Client id in enviroment. To fix this run export SOUNDCLOUD_ALEXA_BRIDGE_CLIENT_ID=1-wdwd-3434-fdf"}
 
+function extractSoundcloudId(url) {
+    var path = url.split('/')
+    return path[path.indexOf('tracks')+1]
+
+}
+
+function getSoundcloudInfo(id, callback) {
+    request('https://api.soundcloud.com/tracks/' + id + '?client_id=' + client_id,
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                callback(JSON.parse(body));
+            }
+        });
+}
+
 app.header(function ($) {
-    console.log($.header)
-    console.log("Header Host", $.header("host"));
-    console.log("Header Authorization", $.header("authorization"));
+    //console.log($.header)
+    //console.log("Header Host", $.header("host"));
+    //console.log("Header Authorization", $.header("authorization"));
     //reject if not authorized
     //$.failure()
     $.return()
 });
 
 app.get('/play', function ($) {
+    if (!!player && !$.query.hasOwnProperty('links')) {
+        player.play();
+        return $.end('continue player');
+    }
     var links = $.query.links.split(",");
     links = links.map((url) => url + "?client_id=" + client_id);
     console.log("links:", links);
@@ -26,10 +46,10 @@ app.get('/play', function ($) {
     } else {
         player = new Player(links);
         player.play();
-        $.end('new player');
+        player.current_track_index = 0;
+        $.end('new player, playing ' + player.list[player.current_track_index]);
     }
 });
-
 
 app.get('/stop', function ($) {
     player.pause();
@@ -42,13 +62,23 @@ app.get('/volume', function ($) {
 });
 
 app.get('/info', function ($) {
-    $.end('info');
+    var url = player.list[player.current_track_index];
+    var id = extractSoundcloudId(url);
+    getSoundcloudInfo(id, function(data) {$.json(data)});
+
 });
 
 app.get('/next', function ($) {
+    if (player.current_track_index >= player.list.length) {
+        $.end('no more tracks');
+    }
     player.next();
-    $.end('next');
+    player.current_track_index += 1;
+    $.end('next ' + player.list[player.current_track_index]);
 });
 
+app.get('/playlist', function($) {
+    $.end(player.list)
+})
 
 function play(source) {}
